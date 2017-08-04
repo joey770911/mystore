6d29c6e9e908a605e904c8324477b711ca0d4bfc
ChenJoey <coke770911@gmail.com>
2017-08-04 15:39:37 +0800

    建立用戶後台管理

--- estore/views.py
+++ estore/views.py
@@ -1,13 +1,20 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.models import Group, User
 from django.urls import reverse
 from django.views import generic
 
 from .models import Product
 
 # Create your views here.
-class ProductList(generic.ListView):
+class ProductList(PermissionRequiredMixin, generic.ListView):
     model = Product
+
+    def has_permission(self):
+        if self.permission_required:
+            return super(ProductList, self).has_permission()
+        else:
+            return True
 
 class ProductDetail(generic.DetailView):
     model = Product
@@ -19,7 +26,7 @@ class ProductCreate(PermissionRequiredMixin, generic.CreateView):
 
     def get_success_url(self):
         messages.success(self.request, '產品已新增')
-        return reverse('product_list')
+        return reverse('dashboard_product_list')
 
 class ProductUpdate(PermissionRequiredMixin, generic.UpdateView):
     permission_required = 'estore.change_product'
@@ -28,4 +35,35 @@ class ProductUpdate(PermissionRequiredMixin, generic.UpdateView):
 
     def get_success_url(self):
         messages.success(self.request, '產品已變更')
-        return reverse('product_update', kwargs=self.kwargs)
+        return reverse('dashboard_product_update', kwargs=self.kwargs)
+
+class UserList(PermissionRequiredMixin, generic.ListView):
+    permission_required = 'auth.change_user'
+    model = User
+    template_name = 'estore/dashboard_user_list.html'
+
+
+class UserAddToStaff(PermissionRequiredMixin, generic.UpdateView):
+    permission_required = 'auth.change_user'
+    model = User
+    fields = []
+
+    def get_success_url(self):
+        if self.request.method == 'POST':
+            group = Group.objects.get(name='estore_staff')
+            group.user_set.add(self.object)
+            messages.success(self.request, '已變更使用者身份為管理者')
+        return reverse('dashboard_user_list')
+
+
+class UserRemoveFromStaff(PermissionRequiredMixin, generic.UpdateView):
+    permission_required = 'auth.change_user'
+    model = User
+    fields = []
+
+    def get_success_url(self):
+        if self.request.method == 'POST':
+            group = Group.objects.get(name='estore_staff')
+            group.user_set.remove(self.object)
+            messages.success(self.request, '已變更使用者身份為一般使用者')
+        return reverse('dashboard_user_list')
